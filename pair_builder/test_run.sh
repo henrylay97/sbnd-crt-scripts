cc7e0ff036f8ffc5f3ee5a6451537e06ab767744
Marco Del Tutto <mdeltutt@fnal.gov>
2022-05-14 09:56:54 -0500

    Add script for testing

---
+++ pair_builder/test_run.sh
@@ -0,0 +1,2 @@
+# ./pair_builder /sbnd/data/CRT/CRT_data/ProdRun20170623_152137-sbndcrt01.1 /sbnd/data/users/mdeltutt/crt_data/test/test01 3
+./pair_builder /sbnd/data/CRT/CRT_data/ProdRun20170623_152137-sbndcrt02.1 /sbnd/data/users/mdeltutt/crt_data/test/test02 3
