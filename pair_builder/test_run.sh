b3a85f1228e1692be51945ea27e8d6a7c17a448d
Marco Del Tutto <mdeltutt@fnal.gov>
2022-09-22 13:49:45 -0500

    Test file

--- pair_builder/test_run.sh
+++ pair_builder/test_run.sh
@@ -1,2 +1,8 @@
-# ./pair_builder /sbnd/data/CRT/CRT_data/ProdRun20170623_152137-sbndcrt01.1 /sbnd/data/users/mdeltutt/crt_data/test/test01 3
-./pair_builder /sbnd/data/CRT/CRT_data/ProdRun20170623_152137-sbndcrt02.1 /sbnd/data/users/mdeltutt/crt_data/test/test02 3
+# Runs the pair builder only on one Run, for both the upstream 01 and downstream 02 panels
+
+# Upstream
+./pair_builder /sbnd/data/CRT/CRT_data/ProdRun20170623_152137-sbndcrt01.1 /sbnd/data/users/mdeltutt/crt_data/test/ProdRun20170623_152137-sbndcrt01.1.beam 3
+
+# Downstream
+./pair_builder /sbnd/data/CRT/CRT_data/ProdRun20170623_152137-sbndcrt02.1 /sbnd/data/users/mdeltutt/crt_data/test/ProdRun20170623_152137-sbndcrt02.1.beam 3
+
