d705f8edd4e10c1011bfee426b747ccf506e6f03
Henry Lay <h.lay@lancaster.ac.uk>
2022-10-12 13:47:26 -0500

    Use same version as local build

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 source /cvmfs/sbnd.opensciencegrid.org/products/sbnd/setup_sbnd.sh
 
-setup sbndcode v09_47_00 -q e20:prof
+setup sbndcode v09_59_00 -q e20:prof
 
 echo
 echo "*********************************"
